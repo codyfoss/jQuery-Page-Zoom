15b2c3ea39422b885f92d7ec53087cc4145d51d2
codyf <cody@demand.cr>
2015-08-10 14:05:40 -0600

    Updated to js-cookie plugin, added an example.

--- README.md
+++ README.md
@@ -1,23 +1,22 @@
 jQuery-Page-Zoom-plugin
 =======================
 
-Allow the user to zoom in or out on the page using links or buttons (anything that will accept an onClick event).
+Allow the user to zoom in or out on the page using links or buttons (anything that will accept an onClick event). If the js-cookie library is available, the zoom level is saved to a cookie. This saves visitors from having to reapply the zoom level on every page load.
 
 ## General Requirements
 
 You'll need to have these libraries included on your page somewhere:
 
 - jQuery: [http://jquery.com/](http://jquery.com/)
-- jquery-cookie: [https://github.com/carhartl/jquery-cookie](https://github.com/carhartl/jquery-cookie)
+- js-cookie (optional): [https://github.com/js-cookie/js-cookie](https://github.com/js-cookie/js-cookie)
 
 ## Usage
 
-Add jQuery and the jquery-cookie and jQuery Page Zoom plugins to your page.
+Add jQuery, js-cookie and jQuery Page Zoom to your page.
 
 	<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-	<script type="text/javascript" src="./javascript/jquery.cookie.js"></script>
+	<script type="text/javascript" src="./javascript/js.cookie.js"></script>
 	<script type="text/javascript" src="./javascript/jquery.page_zoom.js"></script>
-
 
 Set up a few links or buttons on your page that will act as  the controls.
 
@@ -46,7 +45,7 @@ You can optionally pass an array of options to the function to customize how it 
 		min_zoom: .5, // the minimum amount you can zoom out. .5 = 50% zoom
 		zoom_increment: .1, // how much each click zooms in/out. So if the current zoom is 1.3, one click of the zoom out link means 1.3 - 0.1 = 1.2. New zoom level is 1.2
 		current_zoom: 1, // default zoom. don't set this unless you want to force a zoom level on each page load
-		selectors: { 
+		selectors: {
 			zoom_in: '.zoom_in', // the selector for the element(s) that will get the zoom in action for the onclick event
 			zoom_out: '.zoom_out', // the selector for the element(s) that will get the zoom out action for the onclick event
 			zoom_reset: '.zoom_reset' // the selector for the element(s) that will get the reset zoom action for the onclick event
@@ -61,4 +60,4 @@ Pull requests are welcome!
 
 ## License
 
-This library is being provided as is under an MIT license. See LICENSE.txt for the details of that. 
+This library is being provided as is under an MIT license. See LICENSE.txt for the details of that.
--- jquery.page_zoom.js
+++ jquery.page_zoom.js
@@ -1,68 +1,91 @@
 /*!
  * jQuery Page Zoom Plugin v1.0
  *
- * Description: 
+ * Description:
  * Allow the user to zoom in or out on the page using links or buttons (anything that will accept an onClick event).
  *
  */
 
 (function( $ ){
 
-	$.page_zoom = function( options ) {  
+	$.page_zoom = function( options ) {
 
 		var settings = {
 			max_zoom: 1.5,
 			min_zoom: .5,
 			zoom_increment: .1,
 			current_zoom: 1,
-			selectors: { 
-				zoom_in: '.zoom_in', 
-				zoom_out: '.zoom_out', 
+			selectors: {
+				zoom_in: '.zoom_in',
+				zoom_out: '.zoom_out',
 				zoom_reset: '.zoom_reset'
 			}
 		};
-		
+		var cookies_available = false;
+
 		var methods = {
-			
-			// set up 
-		    init : function( options ) { 
+
+			// set up
+		    init : function( options ) {
 
 				if($.isArray(options)){ // if an array was sent, merge it with settings
 
 					settings = $.extend( settings, options);
 
 				}
-		
-				if($.cookie('page_zoom') == null){
-					$.cookie('page_zoom', 1);
+
+				if(typeof(Cookies) != 'undefined'){
+
+					cookies_available = true
+
+				}
+
+				if(cookies_available && Cookies.get('page_zoom') == null){
+
+					Cookies.set('page_zoom', 1);
+
 				} else {
-					settings.current_zoom = parseFloat($.cookie('page_zoom'));
+
+					if(cookies_available) {
+
+						settings.current_zoom = parseFloat(Cookies.get('page_zoom'));
+
+					} else {
+
+						settings.current_zoom = 1;
+
+					}
 
 					if(settings.current_zoom != 1){ // only set zoom if it's not the default zoom
 						methods.set_zoom(settings.current_zoom);
 					}
 				}
-		
+
 				// attached zoom functions to page elements
 				$(settings.selectors.zoom_in).bind('click', methods.zoom_in);
 				$(settings.selectors.zoom_out).bind('click', methods.zoom_out);
 				$(settings.selectors.zoom_reset).bind('click', methods.zoom_reset);
-				
+
 		    },
-			
+
 		    set_zoom : function( zoom_level ) {
-				
+
 				$('body').css({
 					'zoom': zoom_level,
 					'-moz-transform': 'scale(' + zoom_level + ')',
 					'-moz-transform-origin': '0 0'
 				});
-				
+
 				settings.current_zoom = zoom_level;
-				$.cookie('page_zoom', zoom_level);
+
+				if(cookies_available){
+
+					Cookies.set('page_zoom', zoom_level);
+
+				}
 		    },
-			
-		    zoom_in : function( ) { 
+
+		    zoom_in : function( ) {
 
 				var new_zoom = parseFloat(settings.current_zoom + settings.zoom_increment);
 
@@ -72,8 +95,8 @@
 					methods.set_zoom(settings.max_zoom);
 				}
 		    },
-			
-		    zoom_out : function( ) { 
+
+		    zoom_out : function( ) {
 
 				var new_zoom = parseFloat(settings.current_zoom - settings.zoom_increment);
 
@@ -83,15 +106,14 @@
 					methods.set_zoom(settings.min_zoom);
 				}
 		    },
-			
-		    zoom_reset : function( ) { 
+
+		    zoom_reset : function( ) {
 
 				methods.set_zoom(1);
 		    }
 		};
-		
+
 		methods.init();
-		
+
 	};
 })( jQuery );
-
